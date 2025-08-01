0acb9386c30b3f31e4d4c555f40ebc174ca37e73
siddharthkanbargi <siddharthkanbargi74@gmail.com>
2025-08-01 12:55:17 +0530

    Sample app updates

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,18 @@ const client = generateClient<Schema>();
   const [lastname, setLastname] = useState("" as string);
   const [phoneNumber, setPhoneNumber] = useState("" as string);
   const [email, setEmail] = useState("" as string);
-  const [role, setRole] = useState("" as string);
+  const [roleId, setRoleId] = useState("" as string);
+  const [role, setRole] = useState<Array<Schema["Role"]["type"]>>([]);
+
+  function listRoles() {
+    client.models.Role.observeQuery().subscribe({
+      next: (data) => setRole([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listRoles();
+  }, []);
 
   
 function createUser() {
@@ -30,11 +41,12 @@ function createUser() {
       lastname: lastname,
       phonenumber: phoneNumber,
       email: email,
-      roleId: "1",
+      roleId: roleId,
     });
   }
 
   return (
+    <div className="flex flex-col justify-center items-center h-screen">
     <form className="flex flex-col gap-2 w-full max-w-xs">
     {/* <div className="grid gap-6 mb-6 md:grid-cols-2"> */}
         <div>
@@ -54,7 +66,16 @@ function createUser() {
         <label form="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
         <input type="email" id="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
     </div>
+    <div className="mb-2">
+    <select className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label form="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+        {role.map((r) => (
+          <option  label="Role" onSelect={()=>{setRoleId(r.id)}} key={r.id}>{r.role_name}</option>
+        ))}
+      </select>
+      </div>
     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={createUser}>Submit</button>
 </form>
+</div>
   );
 }
